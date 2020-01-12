fa0c9ff81b4bb2b3455701427b1f4c0c1f260033
Adinai949 <adika949@gmail.com>
2020-01-12 22:30:11 +0600

    new file

---
+++ part2_1.py
@@ -0,0 +1,5 @@
+first_name = input("Введите Ваше имя: ").capitalize()
+last_name = input("Введите Вашу фамилию: ").capitalize()
+full_name = first_name + " " + last_name
+message = "Hello," + " " + full_name + ' ' + "!"
+print(message)
